56f0e5f1565adce5bbea714d81840520390680ea
Ian Park <ianpark@Ianui-MacBookPro.local>
2022-04-24 17:34:59 +0900

    #2.6-7 Styles

--- App.js
+++ App.js
@@ -1,14 +1,80 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Dimensions, Text, ScrollView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const { width: SCROLL_WIDTH } = Dimensions.get("window");
 
 function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </ScrollView>
+      <StatusBar style="light" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#6768ac",
+  },
+
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  cityName: {
+    fontSize: 58,
+    fontWeight: "600",
+  },
+
+  weather: {},
+
+  day: {
+    width: SCROLL_WIDTH,
+    alignItems: "center",
+  },
+
+  temp: {
+    marginTop: 50,
+    fontSize: 158,
+    fontWeight: "600",
+  },
+
+  desc: {
+    fontSize: 60,
+  },
+});
 
 export default App;
